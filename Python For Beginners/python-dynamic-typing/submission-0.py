6e10acfb2e55ef83c08eca221b4b01cf35825997
Julio Anccasi <julio.anccasi.dev@gmail.com>
2026-05-10 17:54:28 -0500

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,19 @@
+variable = 10
+
+print(type(variable))
+
+variable = 10.1
+
+print(type(variable))
+
+variable = False
+
+print(type(variable))
+
+variable = 'Julio'
+
+print(type(variable))
+
+variable = ['Julio', 'Anccasi']
+
+print(type(variable))
